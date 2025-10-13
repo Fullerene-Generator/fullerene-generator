9746aac7a999d9f8f3eda228ae668a5ffaa05dc5
kubapoke <q.paczka@gmail.com>
2025-10-13 16:35:38 +0200

    init-fullerene

---
+++ include/fullerene.h
@@ -0,0 +1,23 @@
+﻿#ifndef FULLERENE_H
+#define FULLERENE_H
+#include <memory>
+#include <vector>
+
+enum class node_type {
+    NODE_5,
+    NODE_6,
+  };
+
+struct node {
+    node_type type;
+    std::vector<std::shared_ptr<node>> neighbors;
+};
+
+class dual_fullerene {
+    std::vector<std::shared_ptr<node>> nodes;
+
+public:
+    explicit dual_fullerene(std::vector<std::shared_ptr<node>> nodes);
+};
+
+#endif //FULLERENE_H
---
+++ src/fullerene.cpp
@@ -0,0 +1,7 @@
+﻿#include "fullerene.h"
+
+#include <utility>
+
+dual_fullerene::dual_fullerene(std::vector<std::shared_ptr<node>> nodes) {
+    this->nodes = std::move(nodes);
+}
