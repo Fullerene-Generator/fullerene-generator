b0e940bf54a18903162ef49b5204bcdfb1143279
kubapoke <q.paczka@gmail.com>
2025-11-12 18:03:51 +0100

    added constructions for all 3 base fullerenes

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,14 @@
 
 
 int main() {
-    auto C20 = create_C20_fullerene();
+    auto C20 = create_c20_fullerene();
+    std::cout << "Successfully created C20 fullerene" << std::endl;
+
+    auto C28 = create_c28_fullerene();
+    std::cout << "Successfully created C28 fullerene" << std::endl;
+
+    auto C30 = create_c30_fullerene();
+    std::cout << "Successfully created C30 fullerene" << std::endl;
 
     return 0;
 }
