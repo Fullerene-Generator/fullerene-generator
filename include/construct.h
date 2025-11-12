4e144231027e3e099a42bc8b9cb936173e51c125
kubapoke <q.paczka@gmail.com>
2025-11-12 01:48:49 +0100

    modified fullerene structure

---
+++ include/construct.h
@@ -0,0 +1,26 @@
+﻿#ifndef CONSTRUCT_H
+#define CONSTRUCT_H
+
+#include <fullerene.h>
+#include <vector>
+
+const std::vector<std::vector<unsigned int>> dodecahedron_adjacency = {
+    {1, 4, 5, 2, 3},
+    {0, 3, 7, 6, 4},
+    {0, 5, 9, 8, 3},
+    {0, 2, 8, 7, 1},
+    {0, 1, 6, 10, 5},
+    {0, 4, 10, 9, 2},
+    {1, 7, 11, 10, 4},
+    {1, 3, 8, 11, 6},
+    {2, 9, 11, 7, 3},
+    {2, 5, 10, 11, 8},
+    {4, 6, 11, 9, 5},
+    {6, 7, 8, 9, 10}
+};
+
+dual_fullerene create_C20_fullerene() {
+    return dual_fullerene(dodecahedron_adjacency);
+}
+
+#endif //CONSTRUCT_H
--- include/fullerene.h
+++ include/fullerene.h
@@ -8,16 +8,55 @@ enum class node_type {
     NODE_6,
   };
 
-struct node {
-    node_type type;
-    std::vector<std::shared_ptr<node>> neighbors;
+class base_node {
+protected:
+    std::vector<std::weak_ptr<base_node>> neighbors_;
+    unsigned int id_ = 0;
+
+    explicit base_node(const unsigned int id) : id_(id) {}
+public:
+    virtual ~base_node() = default;
+
+    [[nodiscard]] virtual std::size_t expected_degree() const = 0;
+
+    [[nodiscard]] unsigned int id() const { return id_; }
+
+    void add_neighbor(const std::shared_ptr<base_node>& n);
+
+    [[nodiscard]] std::size_t neighbor_count() const;
+};
+
+class node_5 final : public base_node {
+    explicit node_5(const unsigned int id) : base_node(id) {}
+public:
+    static std::shared_ptr<node_5> create(const unsigned int id) {
+        return std::shared_ptr<node_5>(new node_5(id));
+    }
+
+    [[nodiscard]] std::size_t expected_degree() const override { return 5; }
+};
+
+class node_6 final : public base_node {
+    explicit node_6(const unsigned int id) : base_node(id) {}
+public:
+    static std::shared_ptr<node_6> create(const unsigned int id) {
+        return std::shared_ptr<node_6>(new node_6(id));
+    }
+
+    [[nodiscard]] std::size_t expected_degree() const override { return 6; }
 };
 
 class dual_fullerene {
-    std::vector<std::shared_ptr<node>> nodes;
+    std::vector<std::shared_ptr<node_5>> nodes_5;
+    std::vector<std::shared_ptr<node_6>> nodes_6;
 
 public:
-    explicit dual_fullerene(std::vector<std::shared_ptr<node>> nodes);
+    explicit dual_fullerene(const std::vector<std::vector<unsigned int>>& adjacency);
+
+    [[nodiscard]] const std::vector<std::shared_ptr<node_5>>& get_nodes_5() const noexcept;
+    [[nodiscard]] const std::vector<std::shared_ptr<node_6>>& get_nodes_6() const noexcept;
+
+    [[nodiscard]] std::size_t total_nodes() const noexcept { return nodes_5.size() + nodes_6.size(); }
 };
 
 #endif //FULLERENE_H
