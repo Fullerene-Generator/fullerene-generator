2a2f3ca881e47c711f66e2697b7505c16d14b1fa
kubapoke <q.paczka@gmail.com>
2025-11-14 12:54:00 +0100

    added primal fullerene generation to main for debug/demonstration purposes

--- src/main.cpp
+++ src/main.cpp
@@ -12,5 +12,14 @@ int main() {
     auto C30 = create_c30_fullerene();
     std::cout << "Successfully created C30 fullerene" << std::endl;
 
+    auto C20_primal = C20.to_primal();
+    std::cout << "Successfully converted C20 dual to primal" << std::endl;
+
+    auto C28_primal = C28.to_primal();
+    std::cout << "Successfully converted C28 dual to primal" << std::endl;
+
+    auto C30_primal = C30.to_primal();
+    std::cout << "Successfully converted C30 dual to primal" << std::endl;
+
     return 0;
 }
