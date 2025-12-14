b17b0f2589b38d7f84bb9fedf152fe13281171cd
kubapoke <q.paczka@gmail.com>
2025-12-14 16:42:57 +0100

    added additional embedder tests

--- tests/test_embeddings.cpp
+++ tests/test_embeddings.cpp
@@ -53,6 +53,36 @@ TEST_CASE("Tutte embedding test for base fullerenes", "[embedder]") {
     }
 }
 
+TEST_CASE("Tutte embedding is deterministic", "[embedder]") {
+    const auto d = create_c30_fullerene();
+    const auto f = d.to_primal();
+    const graph g{ f.get_adjacency(), f.get_outer_face_nodes() };
+
+    auto e1 = embedder::compute_tutte(g);
+    auto e2 = embedder::compute_tutte(g);
+
+    REQUIRE(e1.size() == e2.size());
+
+    for (std::size_t i = 0; i < e1.size(); ++i) {
+        REQUIRE(e1[i][0] == e2[i][0]);
+        REQUIRE(e1[i][1] == e2[i][1]);
+    }
+}
+
+TEST_CASE("Outer face vertices lie on unit circle in Tutte embedding", "[embedder]") {
+    const auto d = create_c20_fullerene();
+    const auto f = d.to_primal();
+    const graph g{ f.get_adjacency(), f.get_outer_face_nodes() };
+
+    auto emb = embedder::compute_tutte(g);
+
+    for (unsigned v : g.outer) {
+        double r = std::hypot(emb[v][0], emb[v][1]);
+        REQUIRE(r <= 1 + EPS);
+        REQUIRE(r >= 1 - EPS);
+    }
+}
+
 TEST_CASE("Tutte spherical embedding test for base fullerenes", "[embedder]") {
     const auto d1 = create_c20_fullerene();
     const auto d2 = create_c28_fullerene();
@@ -94,5 +124,20 @@ TEST_CASE("Tutte spherical embedding test for base fullerenes", "[embedder]") {
     for (auto& coords: e3) {
         REQUIRE(sqrt(coords[0]*coords[0] + coords[1]*coords[1] + coords[2]*coords[2]) <= 1 + EPS);
         REQUIRE(sqrt(coords[0]*coords[0] + coords[1]*coords[1] + coords[2]*coords[2]) >= 1 - EPS);
+    }
+}
+
+TEST_CASE("Tutte sphere embedding is deterministic", "[embedder]") {
+    const auto d = create_c20_fullerene();
+    const auto f = d.to_primal();
+    const graph g{ f.get_adjacency(), f.get_outer_face_nodes() };
+
+    auto e1 = embedder::compute_tutte_sphere_mapping(g);
+    auto e2 = embedder::compute_tutte_sphere_mapping(g);
+
+    for (std::size_t i = 0; i < e1.size(); ++i) {
+        REQUIRE(e1[i][0] == e2[i][0]);
+        REQUIRE(e1[i][1] == e2[i][1]);
+        REQUIRE(e1[i][2] == e2[i][2]);
     }
 }
