142d5ea4d57fcae19e86822f8f7e74db1b18e5ec
kubapoke <q.paczka@gmail.com>
2026-01-02 20:17:57 +0100

    b-expansion base

---
+++ include/expansions/b_expansion.h
@@ -0,0 +1,17 @@
+﻿#ifndef FULLERENE_GENERATOR_B_EXPANSION_H
+#define FULLERENE_GENERATOR_B_EXPANSION_H
+#include "base_expansion.h"
+
+
+class b_expansion: base_expansion {
+public:
+    explicit b_expansion(dual_fullerene &G)
+        : base_expansion(G) {
+    }
+
+    [[nodiscard]] bool validate() const override;
+    void apply() override;
+};
+
+
+#endif //FULLERENE_GENERATOR_B_EXPANSION_H
---
+++ src/expansions/b_expansion.cpp
@@ -0,0 +1,1 @@
+﻿#include <expansions/b_expansion.h>
