e41b757da645a333776a2b5efeb4885d756eaa40
Filip <filip.filipkowski6@gmail.com>
2026-01-19 21:15:11 +0100

    clean up

--- include/generators/base_generator.h
+++ include/generators/base_generator.h
@@ -7,14 +7,12 @@
 
 class base_generator {
 public:
-    std::map<size_t, int> counts;
     virtual ~base_generator() = default;
     virtual void generate(std::size_t up_to) = 0;
     virtual void register_and_emit(dual_fullerene& G) {
         G.register_id();
         auto P = G.to_primal();
-       // std::cout << P << std::flush;
-        counts[P.get_size()]++;
+        std::cout << P << std::flush;
     }
 };
 
--- src/generators/main_generator.cpp
+++ src/generators/main_generator.cpp
@@ -56,9 +56,6 @@ void main_generator::generate(std::size_t up_to)
         register_and_emit(G);
     }
 
-    for (auto chuj : counts) {
-        std::cout << "vertices: " << chuj.first << " graphs: " << chuj.second << '\n';
-    }
 }
 
 int main_generator::bound_by_vertex_count_l(const dual_fullerene& G, std::size_t up_to)
